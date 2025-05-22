c0102a621857cdc70bbd2739bab38f7d0f68199b
adriana.erazo <adriana.erazo@udea.edu.co>
2025-05-21 21:01:01 -0500

    Implementacion validacion ingreso usuario, metodo para el mismo uso dentro de cada clase (Anfitrion, Huesped)

---
+++ DesafioII/utilidades.h
@@ -0,0 +1,15 @@
+#ifndef UTILIDADES_H
+#define UTILIDADES_H
+
+#include <string>
+
+using namespace std;
+
+bool rangeValidation(int numero, int intIni, int intFin);
+
+int intValidation(int limInf,int limSup);
+
+void mostrarMenuAnfitrion();
+
+void mostrarMenuHuesped();
+#endif // UTILIDADES_H
